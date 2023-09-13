dd7c2f2be3f12419242ecab2543e21b00e4127f8
mp1rkin <144093817+mp1rkin@users.noreply.github.com>
2023-09-13 09:28:58 +0300

    Add files via upload

---
+++ ConsoleApplication5/ConsoleApplication5.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+#include <stdlib.h>
+#include <windows.h>
+
+using namespace std;
+
+
+int main()
+
+{
+    // hello!!!
+
+    cout << "\t\t\t\t\t\t\t HELLO !!!\n";
+
+    //kvadrat
+     
+    cout << endl << endl << endl << endl << endl;
+    cout << endl << endl << endl << endl << endl;
+    cout << "\t\t\t\t\t\t------------------" << endl;
+    cout << "\t\t\t\t\t\t|                |" << endl;
+    cout << "\t\t\t\t\t\t|                |" << endl;
+    cout << "\t\t\t\t\t\t|                |" << endl;
+    cout << "\t\t\t\t\t\t|                |" << endl;
+    cout << "\t\t\t\t\t\t|                |" << endl;
+    cout << "\t\t\t\t\t\t|                |" << endl;
+    cout << "\t\t\t\t\t\t|                |" << endl;
+    cout << "\t\t\t\t\t\t------------------" << endl;
+   
+    //anketa 
+
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    cout << "       Прізвище : Станевий" << endl;
+    cout << "           Ім'я :  Евгеній" << endl;
+    cout << "          Стать : чоловіча" << endl;
+    cout << "Дата народження : 2006-08-30" << endl;
+    cout << "         Адреса : Павлоград вул.Відродження 16" << endl;
+    cout << " Номер телефону : +380 93 325 9577" << endl;
+    cout << " Місце навчання : ФПМ, ДНУ ім. Олеся Гончара" << endl;
+    cout << "         Група  : ПА-23-1" << endl;
+    cout << "          Хоббі : програмування" << endl; 
+    system("pause");
+    
+    
+
+
+
+    
+
+
+    
+    
+    
+}
+
+
